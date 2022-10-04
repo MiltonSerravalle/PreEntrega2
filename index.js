8c815ae205236c47c9b3968e06df23566fa70bdc
Milton Serravalle <milton.serravalle@hotmail.com>
2022-10-04 18:52:49 -0300

    Agrega elementos 2

---
+++ index.js
@@ -0,0 +1,32 @@
+class indumentaria {
+    constructor(id, imagen, producto, precio ){
+    this.id = id;
+    this.imagen = imagen;
+    this.producto = producto;
+    this.precio = precio;
+}
+}
+
+const productos = [
+    {
+        id: 1,
+        imagen: "./multimedia/Britney.jpg",
+        nombre: "Remera Britney",
+        precio: 3000
+    },
+    {
+        id: 2,
+        imagen: "./multimedia/Pibes.jpg",
+        nombre: "Remera Pibes Chorros",
+        precio: 3000
+    },
+    {
+        id: 3,
+        imagen: "./multimedia/buzo2.jpg",
+        nombre: "Buzo Gorillaz",
+        precio: 4500
+    }
+]
+
+
+

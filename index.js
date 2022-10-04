a4f3ae309ebba1d15517bda92ce16492e729da8d
Milton Serravalle <milton.serravalle@hotmail.com>
2022-10-04 20:54:29 -0300

    3er commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const listaProductos = document.getElementById("container")
 
 class indumentaria {
-    constructor(id, imagen, producto, precio ){
+    constructor(id, imagen, nombre, precio ){
     this.id = id;
     this.imagen = imagen;
-    this.producto = producto;
+    this.nombre = nombre;
     this.precio = precio;
 }
 }
@@ -30,6 +30,8 @@ const productos = [
     }
 ]
 
+let carroCompras = [];
+
 productos.forEach((item) => {
 let card = document.createElement("div")
 card.innerHTML = `
@@ -40,6 +42,31 @@ card.innerHTML = `
 `
 container.append(card);
 
-})
+const button = document.getElementById(item.id);
+button.addEventListener("click", () => agregaCarrito(item));
+});
+
+const comprarProducto = (item) => {
+    carroCompras.push(item)
+  }
+
+  const agregaCarrito = (item) => {
+    let productoStock = carroCompras.find((producto) => productos.id === item.id);
+    if (productoStock !== undefined) {
+      productoStock.precio = productoStock.precio + item.precio;
+      productoStock.cantidad = productoStock.cantidad + 1;
+    } else {
+      carroCompras.push({
+        id: item.id,
+        nombre: item.nombre,
+        precio: item.precio,
+        cantidad: 1,
+      });
+    }
+  };
+  
+  let carrito = document.getElementById("numberCart");
+  carrito.innerHTML = `${carroCompras.length}`;
+  
 
 
